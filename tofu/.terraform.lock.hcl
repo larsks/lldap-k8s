769b321def1a74ea963e181ebd7cae82c43aad60
Lars Kellogg-Stedman <lars@oddbit.com>
2026-07-30 08:04:01 -0400

    Add example of using lldap terraform provider

---
+++ tofu/.terraform.lock.hcl
@@ -0,0 +1,16 @@
+# This file is maintained automatically by "tofu init".
+# Manual edits may be lost in future updates.
+
+provider "registry.opentofu.org/tasansga/lldap" {
+  version = "0.4.1"
+  hashes = [
+    "h1:U0+ALTLGGPXd/z5GDYVp7AkXk6WXSrgBsEa577GCVXU=",
+    "zh:07d6c46f74ada743baa6c60868125d71fd5a59e81253f9100ac9e52144db0ff5",
+    "zh:2c64c625450c40331610b9cb82190a439b7d68ee006c4d08da496c00dac869ff",
+    "zh:433c49bd01d23c304d1377bcf39599a00a3d612b51a0a5dd4aed8c1c389979bd",
+    "zh:8b2bcf56da2e293484d8b987350a5c5e90ccea47e92f95b372be2b89ddc7339e",
+    "zh:9da2b41396d7fe7e11e655d8b4df6af06bfa14a23b60d06651b78ea7a225d44a",
+    "zh:c10a40cc3479b2a63464f62401cf744a753a6cf33d5566371ac311ce4e04de93",
+    "zh:d3f8f4208589b5167f7c0994bdabc352e04b20de5efe0a74e3153b4308475eb8",
+  ]
+}
---
+++ tofu/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_version = ">= 1.9.0"
+  required_providers {
+    lldap = {
+      source = "tasansga/lldap"
+    }
+  }
+}
+
+variable "lldap_admin_password" {
+  type = string
+  sensitive = true
+}
+
+provider "lldap" {
+  http_url = "http://localhost:17170"
+  ldap_url = "ldap://localhost:3890"
+  username = "admin"
+  password = var.lldap_admin_password
+  base_dn  = "dc=example,dc=com"
+}
+
+resource "lldap_user_attribute" "ssh-key" {
+  name           = "ssh-key"
+  is_editable    = true
+  attribute_type = "STRING"
+}
+
+resource "lldap_user_attribute" "wireguard-key" {
+  name           = "wireguard-key"
+  is_editable    = true
+  attribute_type = "STRING"
+}
+
+resource "lldap_user" "larsks" {
+  username     = "larsks"
+  email        = "lars@redhat.com"
+  display_name = "Lars Kellogg-Stedman"
+  first_name   = "Lars"
+  last_name    = "Kellogg-Stedman"
+}
+
+resource "lldap_user_attribute_assignment" "larsks-ssh-key" {
+  user_id      = lldap_user.larsks.id
+  attribute_id = lldap_user_attribute.ssh-key.id
+  value        = [file("ssh-keys/larsks")]
+}
+
+resource "lldap_user_attribute_assignment" "larsks-wireguard-key" {
+  user_id      = lldap_user.larsks.id
+  attribute_id = lldap_user_attribute.wireguard-key.id
+  value        = [file("wireguard-keys/larsks")]
+}
